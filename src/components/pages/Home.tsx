97071d4be499fe9af3ead49f7af8cdb94a8f94ed
Luke Bates <lnbates20@gmail.com>
2024-06-22 12:57:17 -0400

    Added spinning 3D geometry

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,16 +1,17 @@
 
 import './Home.css'
 import Popup from '@/components/common/Popup'
+import BackgroundGL from '@/components/canvas/BackgroundGL'
 import LU32Img from '@/assets/images/LU32.png'
 
 function Home() {
-  
   return (
     <div>
       <Popup title="NOTICE" width={256} height={300}>
         <img src={LU32Img} alt="LU32" />
         <br />
         <b>This domain is maintained by LB.</b>
+        <BackgroundGL />
       </Popup>
     </div>
   )

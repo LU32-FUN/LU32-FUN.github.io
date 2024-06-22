16925528aab3fabbcbaa5a0c40dbe37ba9030261
Luke Bates <lnbates20@gmail.com>
2024-06-22 11:53:31 -0400

    Added logo to demo popup message

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,7 +1,7 @@
 
 import './Home.css'
 import Popup from '@/components/common/Popup'
-import LU32Img from '$/assets/lu32.png'
+import LU32Img from '@/assets/images/LU32.png'
 
 function Home() {
   
@@ -11,6 +11,8 @@ function Home() {
       <p>Welcome to the Home page!</p>
       
       <Popup title="NOTICE" width={256} height={300}>
+        <img src={LU32Img} alt="LU32" />
+        <br />
         <b>This domain is maintained by LB.</b>
       </Popup>
     </div>

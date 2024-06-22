8caddb681f17fbd8d722dac05488ebba3bf468f1
Luke Bates <lnbates20@gmail.com>
2024-06-22 11:59:24 -0400

    Removed placeholder text

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,9 +7,6 @@ function Home() {
   
   return (
     <div>
-      <h1>Home</h1>
-      <p>Welcome to the Home page!</p>
-      
       <Popup title="NOTICE" width={256} height={300}>
         <img src={LU32Img} alt="LU32" />
         <br />

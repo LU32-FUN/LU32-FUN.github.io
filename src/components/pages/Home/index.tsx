626b49b65ecb6c4d9f76705222f389c6f11c4f52
Luke Bates <lnbates20@gmail.com>
2024-06-24 22:57:05 -0400

    Added proceed and decline buttons

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -10,15 +10,22 @@ function Home() {
     <div>
       <SpaceRenderer width={window.innerWidth} height={window.innerHeight} />
       
-      <Popup title="NOTICE" width={256} height={300}>
+      <Popup title="NOTICE" width={300} height={256}>
         
-        <Container align='center' justify='center'>
-          <img src={LU32Img} alt="LU32" class={styles['logo']} />
-        </Container>
-        
-        <Container align='center'>
-          This domain is maintained by LB.
-        </Container>
+          <Container align='center' justify='center'>
+            <img src={LU32Img} alt="LU32" class={styles['logo']} />
+          </Container>
+          
+          <Container align='center'>
+            This domain is maintained by LB.
+          </Container>
+          
+          <Container align='center'>
+            <div class={styles['button-container']}>
+              <button class={styles.button}> Decline </button>
+              <button class={styles.button}> Proceed </button>
+            </div>
+          </Container>
         
       </Popup>
     </div>

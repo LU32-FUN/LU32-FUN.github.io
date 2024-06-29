1b7b0716765ffa46f7cdce6aed8521d42c0b3f55
Luke Bates <lnbates20@gmail.com>
2024-06-29 12:56:58 -0400

    Added 'about' icon

--- src/components/apps/Welcome/index.tsx
+++ src/components/apps/Welcome/index.tsx
@@ -1,11 +1,12 @@
 
 import AppIcon from '@/components/apps/AppIcon'
+import AboutIcon from '@/assets/icons/about.png'
 
 function Welcome() {
   
   return (
     <div>
-      <AppIcon name="About" />
+      <AppIcon name="About" icon={AboutIcon} />
     </div>
   )
 }
